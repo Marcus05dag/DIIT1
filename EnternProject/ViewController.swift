f775f3da61c62f5ce8e6c98c72d6016d18b39917
Marcus05dag <2pac_ru@mail.ru>
2020-05-09 21:18:26 +0300

    Initial Commit

---
+++ EnternProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EnternProject
+//
+//  Created by Мухтарпаша on 09.05.2020.
+//  Copyright © 2020 Magomed Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
