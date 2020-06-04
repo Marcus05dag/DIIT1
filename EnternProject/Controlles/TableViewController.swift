191429b29a4dcdf53b764515dc7a222bd393a211
Marcus05dag <2pac_ru@mail.ru>
2020-06-04 17:44:44 +0300

    04.06.2020 (party2)

--- EnternProject/Controlles/TableViewController.swift
+++ EnternProject/Controlles/TableViewController.swift
@@ -7,27 +7,55 @@
 //
 
 import UIKit
+import  CoreData
 
-class TableViewController: UITableViewController {
+class TableViewController: UITableViewController{
+    
+    var man:[NSManagedObject] = []
 
+        //MARK:ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .red
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         
         conficureButtonPlus()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        fetchReq()
+        
+        
+    }
+    //MARK:FetchReq
+    fileprivate func fetchReq () {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else  {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person")
+        
+        do {
+            man = try managedContext.fetch(fetchRequest)
+        }catch let error as NSError {
+            print("Could not fetch, \(error) , \(error.userInfo)")
+        }
+        
+    }
     
-    
-    
+    //MARK:configureButtonPlus
     fileprivate func conficureButtonPlus() {
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "Icon-App-20x20-1"), style: .plain, target: self, action: #selector(handlePlus))
         
         
     }
     
-    
+    //MARK:HandlePlus
     @objc fileprivate func handlePlus () {
         print("button plus")
         let alert = UIAlertController(title: "Добавить имя", message: "Добавить новое имя", preferredStyle: .alert)
@@ -52,29 +80,73 @@ class TableViewController: UITableViewController {
         
         
     }
+    
+    //MARK:SAVE
+    fileprivate func save (name:String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)!
+        
+        let person = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        
+        person.setValue(name, forKey: "name")
+        
+        
+        do {
+            try managedContext.save()
+            man.append(person)
+        } catch let error as NSError {
+            print("Could not save,\(error), \(error.userInfo)")
+        }
+        
+    }
 
+    
+    
+    
+    
+    
+    
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return
+//    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return man.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let person = man[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = person.value(forKeyPath: "name") as? String
 
         return cell
     }
-    */
-
+    
+    //MARK:DELETE
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            tableView.beginUpdates()
+            man.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            tableView.reloadData()
+            tableView.endUpdates()
+            
+            
+        }
+        
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
